ecd1fee40d78976684a4dc6d2b844e417ebce9db
Jared Jennings <jjennings@fastmail.fm>
2015-07-24 16:46:22 -0500

    add some files from `puppet module generate`

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# == Class: no_ext_acl
+#
+# This class does nothing. The custom resource type `no_ext_acl`, on
+# the other hand, removes extended access control lists from files and
+# directories under Linux and OS X. See the README for documentation.
+#
+# === Authors
+#
+# Jared Jennings <jjennings@fastmail.fm>
+#
+# === Copyright
+#
+# Copyright 2015 Jared Jennings
+#
+
+class no_ext_acl {
+}
